187a67257145d8e7020a8a1f0802174d217788a9
Andrew Ang <andrewang@ikegps.com>
2016-01-30 19:04:20 +1300

    fixed crash bug

--- app/src/main/java/com/torchapp/andrew/torchapp/MainActivity.java
+++ app/src/main/java/com/torchapp/andrew/torchapp/MainActivity.java
@@ -21,12 +21,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         boolean hasFlash = this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
         setContentView(R.layout.activity_main);
+        this.cam = Camera.open();
+        this.params = this.cam.getParameters();
         this.torchButton = (Button) findViewById(R.id.torchButton);
         this.torchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                cam = Camera.open();
-                params = cam.getParameters();
+                //cam = Camera.open();
+                //params = cam.getParameters();
                 if(!torchState){
                     turnOnFlash();
                 }
@@ -41,17 +43,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void turnOnFlash(){
-        this.params = cam.getParameters();
+        //this.params = cam.getParameters();
         this.params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
         this.cam.setParameters(params);
-        cam.startPreview();
+        this.cam.startPreview();
         this.torchState = true;
         torchButton.setText("Turn Off");
     }
 
     private void turnOffFlash(){
+        this.params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        this.cam.setParameters(this.params);
         cam.stopPreview();
-        cam.release();
+        //cam.release();
         this.torchState = false;
         torchButton.setText("Turn On");
     }
